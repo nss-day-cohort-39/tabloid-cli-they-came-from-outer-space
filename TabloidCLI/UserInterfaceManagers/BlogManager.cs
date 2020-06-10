efa1da0b732d3575c79261c655d43b00d97f6b0e
calvincurry22 <calvincurry22@gmail.com>
2020-06-10 16:03:04 -0500

    Created Blog Repo, added Execute and List methods to BlogManager

--- TabloidCLI/UserInterfaceManagers/BlogManager.cs
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -1,10 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using TabloidCLI.Models;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
-    class BlogManager
+    public class BlogManager : IUserInterfaceManager
     {
+        private readonly IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private string _connectionString;
+
+
+        public BlogManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _blogRepository = new BlogRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Blog Menu");
+            Console.WriteLine(" 1) List Blogs");
+            Console.WriteLine(" 2) Add Blog");
+            Console.WriteLine(" 3) Edit Blog");
+            Console.WriteLine(" 4) Remove Blog");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                //case "2":
+                //    Add();
+                //    return this;
+                //case "3":
+                //    Edit();
+                //    return this;
+                //case "4":
+                //    Remove();
+                //    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Blog> blogs = _blogRepository.GetAll();
+            foreach (Blog blog in blogs)
+            {
+                Console.WriteLine(blog);
+            }
+        }
+
     }
 }

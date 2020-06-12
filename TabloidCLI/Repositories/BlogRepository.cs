6e1a56b77a207ae87f69d61c3e56de26f6676280
Brennen Tatum <bmar566@gmail.com>
2020-06-12 13:27:22 -0500

    added view blog details

--- TabloidCLI/Repositories/BlogRepository.cs
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -135,21 +135,6 @@ namespace TabloidCLI
                 }
             }
         }
-        public void InsertTag(Blog blog, Tag tag)
-        {
-            using (SqlConnection conn = Connection)
-            {
-                conn.Open();
-                using (SqlCommand cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = @"INSERT INTO BlogTag (BlogId, TagId)
-                                                       VALUES (@blogId, @tagId)";
-                    cmd.Parameters.AddWithValue("@blogId", blog.Id);
-                    cmd.Parameters.AddWithValue("@tagId", tag.Id);
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
 
         public void DeleteTag(int blogId, int tagId)
         {
--- TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
+++ TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
@@ -53,85 +53,9 @@ namespace TabloidCLI.UserInterfaceManagers
                 default:
                     Console.WriteLine("Invalid Selection");
                     return this;
-                  
+
             }
         }
 
-        private void View()
-        {
-            Blog blog = _blogRepository.Get(_blogId);
-            Console.WriteLine($"Title: {blog.Title}");
-            Console.WriteLine($"Url: {blog.Url}");
-            Console.WriteLine("Tags:");
-            foreach (Tag tag in blog.Tags)
-            {
-                Console.WriteLine(" " + tag);
-            }
-            Console.WriteLine();
-        }
-
-        private void ViewPosts()
-        {
-            List<Post> posts = _postRepository.GetByBlogId(_blogId);
-            foreach (Post post in posts)
-            {
-                Console.WriteLine(post);
-            }
-            Console.WriteLine();
-        }
-
-        private void AddTag()
-        {
-            Blog blog = _blogRepository.Get(_blogId);
-
-            Console.WriteLine($"Which tag would you like to add to {blog.Title}?");
-            List<Tag> tags = _tagRepository.GetAll();
-
-            for (int i = 0; i < tags.Count; i++)
-            {
-                Tag tag = tags[i];
-                Console.WriteLine($" {i + 1}) {tag.Name}");
-            }
-            Console.Write("> ");
-
-            string input = Console.ReadLine();
-            try
-            {
-                int choice = int.Parse(input);
-                Tag tag = tags[choice - 1];
-                _blogRepository.InsertTag(blog, tag);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Invalid Selection. Won't add any tags.");
-            }
-        }
-
-        private void RemoveTag()
-        {
-            Blog blog = _blogRepository.Get(_blogId);
-
-            Console.WriteLine($"Which tag would you like to remove from {blog.Title}?");
-            List<Tag> tags = blog.Tags;
-
-            for (int i = 0; i < tags.Count; i++)
-            {
-                Tag tag = tags[i];
-                Console.WriteLine($" {i + 1}) {tag.Name}");
-            }
-            Console.Write("> ");
-
-            string input = Console.ReadLine();
-            try
-            {
-                int choice = int.Parse(input);
-                Tag tag = tags[choice - 1];
-                _blogRepository.DeleteTag(blog.Id, tag.Id);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Invalid Selection. Won't remove any tags.");
-            }
-        }
     }
 }
